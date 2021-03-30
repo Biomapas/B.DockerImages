917ad56cb52219a071c3edecadddf295b90c23fc
laimonassutkus <laimonas.sutkus@gmail.com>
2021-03-30 11:48:33 +0300

    Do not delte dist-info.

--- CiCdFull/python_utils/clean_python.sh
+++ CiCdFull/python_utils/clean_python.sh
@@ -9,5 +9,8 @@ export PYTHONDONTWRITEBYTECODE=1
 # Find all python caches and metadata files. Delete all of them.
 find . -type f -name "*.py[co]" -delete
 find . -type d -name "__pycache__" -exec rm -rf {} +
-find . -type d -name "*.dist-info" -exec rm -rf {} +
 find . -type d -name "*.egg-info" -exec rm -rf {} +
+
+# This removal of dist-info directories can actually break some of the libraries.
+# Try not to delete them.
+# find . -type d -name "*.dist-info" -exec rm -rf {} +
