2e3877a4a10d974bf35bc11632d6b135faa99f92
laimonassutkus <laimonas.sutkus@gmail.com>
2021-05-02 08:24:00 +0300

    Add magic destroy command.

---
+++ CiCdFull/magic_utils/magic_cdk_destroy.sh
@@ -0,0 +1,20 @@
+# ------------------------------------------------
+# Destroys infrastructure by executing `cdk destroy "*"` command.
+# ------------------------------------------------
+
+set -e
+set -o pipefail
+
+# If a virtual environment directory is already present, simply source it.
+if [ -d "./venv" ]; then
+  source venv/bin/activate
+# However, if virtual environment is not present, create it, and then source it.
+else
+  source /usr/local/python_utils/new_venv.sh
+  /usr/local/magic_utils/magic_pip_install.sh
+fi
+
+/usr/local/node_utils/install_aws_cdk.sh
+source /usr/local/python_utils/clean_python.sh
+
+./node_modules/aws-cdk/bin/cdk destroy "*"
